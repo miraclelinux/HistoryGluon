1f43327117181a4401817f52ade726001c421b7d
Takuro Ashie <ashie@clear-code.com>
2013-03-29 18:19:24 +0900

    Add range_query method to ruby binding.

--- client-ruby-ext/rb-history-gluon.c
+++ client-ruby-ext/rb-history-gluon.c
@@ -2,6 +2,7 @@
 #include <history-gluon.h>
 
 VALUE rb_cHistoryGluon;
+static VALUE sym_data_id, sym_sec, sym_ns, sym_type, sym_value, sym_length;
 
 typedef struct _HglRubyPtr
 {
@@ -68,6 +69,67 @@ add_string(VALUE self, VALUE id, VALUE sec, VALUE ns, VALUE data)
 }
 
 static VALUE
+create_gluon_data_value(history_gluon_data_t *gluon_data)
+{
+    VALUE data = rb_hash_new();
+
+    rb_hash_aset(data, sym_data_id, ULL2NUM(gluon_data->id));
+    rb_hash_aset(data, sym_sec,     LL2NUM(gluon_data->ts.tv_sec));
+    rb_hash_aset(data, sym_ns,      LL2NUM(gluon_data->ts.tv_nsec));
+    rb_hash_aset(data, sym_type,    INT2NUM(gluon_data->type));
+
+    if (gluon_data->type == HISTORY_GLUON_TYPE_FLOAT) {
+        rb_hash_aset(data, sym_value, rb_float_new(gluon_data->v.fp));
+    } else if (gluon_data->type == HISTORY_GLUON_TYPE_UINT) {
+        rb_hash_aset(data, sym_value, ULL2NUM(gluon_data->v.uint));
+    } else if (gluon_data->type == HISTORY_GLUON_TYPE_STRING) {
+        rb_hash_aset(data, sym_value, rb_str_new2(gluon_data->v.string));
+    } else if (gluon_data->type == HISTORY_GLUON_TYPE_BLOB) {
+        rb_hash_aset(data, sym_value, rb_str_new2((char*)gluon_data->v.blob));
+    } else {
+        rb_raise(rb_eRuntimeError,
+                 "%s: %d: Unknown data type: %d",
+                 __FILE__, __LINE__, gluon_data->type);
+    }
+
+    return data;
+}
+
+static VALUE
+range_query(VALUE self, VALUE id, VALUE sec0, VALUE ns0, VALUE sec1, VALUE ns1,
+	    VALUE sort_request, VALUE num_max_entries)
+{
+    HglRubyPtr *hgl;
+    struct timespec ts0 = { NUM2LL(sec0), NUM2LL(ns0) };
+    struct timespec ts1 = { NUM2LL(sec1), NUM2LL(ns1) };
+    history_gluon_result_t result;
+    history_gluon_data_array_t *array = NULL;
+    unsigned long i;
+    VALUE value_array;
+
+    Data_Get_Struct(self, HglRubyPtr, hgl);
+    result = history_gluon_range_query(hgl->ctx,
+				       NUM2ULL(id),
+				       &ts0, &ts1,
+				       NUM2INT(sort_request),
+				       NUM2ULL(num_max_entries),
+				       &array);
+    if (result != HGL_SUCCESS) {
+	/* FIXME: raise suitable exception */
+        rb_raise(rb_eRuntimeError,
+                 "Failed to call history_gluon_range_query: %d",
+                 result);
+	return Qnil;
+    }
+
+    value_array = rb_ary_new();
+    for (i = 0; i < array->num_data; i++)
+	rb_ary_push(value_array, create_gluon_data_value(array->array[i]));
+
+    return value_array;
+}
+
+static VALUE
 initialize(VALUE self, VALUE database_name, VALUE server_name, VALUE port)
 {
     HglRubyPtr *ptr;
@@ -88,4 +150,12 @@ Init_historygluon(void)
     rb_define_method(rb_cHistoryGluon, "add_uint", add_uint, 4);
     rb_define_method(rb_cHistoryGluon, "add_float", add_float, 4);
     rb_define_method(rb_cHistoryGluon, "add_string", add_string, 4);
+    rb_define_method(rb_cHistoryGluon, "range_query", range_query, 7);
+
+    sym_data_id = ID2SYM(rb_intern("id"));
+    sym_sec	= ID2SYM(rb_intern("sec"));
+    sym_ns	= ID2SYM(rb_intern("ns"));
+    sym_type	= ID2SYM(rb_intern("type"));
+    sym_value	= ID2SYM(rb_intern("value"));
+    sym_length	= ID2SYM(rb_intern("length"));
 }
--- client-ruby-ext/test/test_history_gluon.rb
+++ client-ruby-ext/test/test_history_gluon.rb
@@ -9,4 +9,20 @@ class HistoryGluonTestCase < Test::Unit::TestCase
     hgl = HistoryGluon.new("zabbix", "localhost", 0)
     assert_equal(hgl.class, HistoryGluon)
   end
+
+  def test_range_query
+    hgl = HistoryGluon.new("zabbix", "localhost", 0)
+    hgl.add_uint(1, 0, 0, 5)
+    hgl.add_uint(1, 21, 1, 5)
+    hgl.add_uint(1, 1, 0, 5)
+    hgl.add_float(1, 10, 10, 5.0)
+    hgl.add_string(1, 20, 20, "5.0")
+    actual = hgl.range_query(1, 1, 0, 21, 0, 0, 100)
+    expected = [
+                { :id => 1, :type => 2, :sec => 1,  :ns => 0,  :value => 5},
+                { :id => 1, :type => 0, :sec => 10, :ns => 10, :value => 5.0},
+                { :id => 1, :type => 1, :sec => 20, :ns => 20, :value => "5.0"},
+               ]
+    assert_equal(expected, actual);
+  end
 end
